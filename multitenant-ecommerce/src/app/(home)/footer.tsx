e01daa62851891f5b5cc4a264401f42c64616991
Marco Ponce <marcpon8@gmail.com>
2025-05-17 20:11:34 -0400

    added the base to the home layout as well as the mobile implementation for mobile users

---
+++ multitenant-ecommerce/src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="flex border-t justify-between font-medium p-6">
+            <div>
+                <p>poncema4, Inc.</p>
+            </div>
+            </footer>
+    );
+};
