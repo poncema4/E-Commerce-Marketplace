1de93cabb11075f1822b54cd9764658c23e31498
Marco Ponce <marcpon8@gmail.com>
2025-06-09 12:39:04 -0400

    added products functionality and inclusive to categories and subcategories

--- multitenant-ecommerce/src/modules/home/ui/components/search-filters/index.tsx
+++ multitenant-ecommerce/src/modules/home/ui/components/search-filters/index.tsx
@@ -43,7 +43,7 @@ export const SearchFilters = () => {
     );
 };
 
-export const SearchFiltersLoading = () => {
+export const SearchFiltersSkeleton = () => {
     return (
         <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full" style={{
             backgroundColor: "F5F5F5",
