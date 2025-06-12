a96a9afb8db984163384640e6acd88d7a4726d5f
Marco Ponce <marcpon8@gmail.com>
2025-06-11 20:14:02 -0400

    added a functionality to the filter for min and max cost for items and continue to develop it using an api filter sorting mechanism for easier access and url manipulation

--- multitenant-ecommerce/src/modules/categories/server/procedures.ts
+++ multitenant-ecommerce/src/modules/categories/server/procedures.ts
@@ -20,7 +20,6 @@ export const categoriesRouter = createTRPCRouter({
             subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
                 // Because of "depth: 1" we are confident that doc will be a type of "Category"
                 ...(doc as Category),
-                subcategories: undefined,
             }))
         }));
     
