983520954d1db8b0672febef9d199c1d20e90d6b
Marco Ponce <marcpon8@gmail.com>
2025-06-14 15:26:35 -0400

    api filter and sorting:

--- multitenant-ecommerce/src/modules/products/server/procedures.ts
+++ multitenant-ecommerce/src/modules/products/server/procedures.ts
@@ -1,7 +1,8 @@
 import { Category } from "@/payload-types";
 import { baseProcedure, createTRPCRouter} from "@/trpc/init";
-import { Where } from "payload";
+import { Sort, Where } from "payload";
 import z from "zod";
+import { sortValues } from "../search-params";
 
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure
@@ -10,10 +11,25 @@ export const productsRouter = createTRPCRouter({
             category: z.string().nullable().optional(),
             minPrice: z.string().nullable().optional(),
             maxPrice: z.string().nullable().optional(),
+            tags: z.array(z.string()).nullable().optional(),
+            sort: z.enum(sortValues).nullable().optional(),
         }),
     ) 
     .query(async ({ ctx , input}) => {
         const where: Where = {};
+        let sort: Sort = "-createdAt";
+
+        if (input.sort === "default") {
+            sort = "-createdAt";
+        }
+
+        if (input.sort === "newest") {
+            sort = "-createdAt";
+        }
+
+        if (input.sort === "oldest") {
+            sort = "+createdAt";
+        }
 
         if (input.minPrice && input.maxPrice) {
             where.price = {
@@ -66,10 +82,17 @@ export const productsRouter = createTRPCRouter({
                 }
             }
 
+        if (input.tags && input.tags.length > 0) {
+            where['tags.name'] = {
+                in: input.tags,
+            };
+        }
+
         const data = await ctx.db.find({
             collection: 'products',
             depth: 1, // Populate "category" and "image" fields
             where,
+            sort,
     });
 
     return data
