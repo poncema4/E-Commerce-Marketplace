5381bbbfda4b366c843c3875f410c5aa1b259a23
Marco Ponce <marcpon8@gmail.com>
2025-06-16 19:20:14 -0400

    multi-tenancy

--- multitenant-ecommerce/src/modules/home/ui/components/search-filters/subcategory-menu.tsx
+++ multitenant-ecommerce/src/modules/home/ui/components/search-filters/subcategory-menu.tsx
@@ -6,13 +6,11 @@ import { CategoriesGetManyOutput } from '@/modules/categories/types';
 interface Props {
     category: CategoriesGetManyOutput[1];
     isOpen: boolean;
-    position: {top: number; left: number};
 }
 
 export const SubcategoryMenu = ({ 
     category, 
     isOpen, 
-    position 
 }: Props) => { 
     if (!isOpen || !category.subcategories || category.subcategories.length === 0) {
         return null;
@@ -21,10 +19,10 @@ export const SubcategoryMenu = ({
     const backgroundColor = category.color || '#F5F5F5';
     return (
         <div 
-        className="fixed z-100"
+        className="absolute z-100"
         style={{
-            top: position.top,
-            left: position.left,
+            top: "100%",
+            left: 0,
         }}>
             {/* Invisible bridge to maintain hover */}
             <div className="h-4 w-60" />
--- multitenant-ecommerce/src/seed.ts
+++ multitenant-ecommerce/src/seed.ts
@@ -140,6 +140,32 @@ const categories = [
 const seed = async () => {
     const payload = await getPayload({ config });
 
+    // Create admin tenant
+    const adminTenant = await payload.create({
+      collection: "tenants",
+      data: {
+        name: "admin",
+        slug: "admin",
+        stripeAccountId: "admin",
+      },
+    });
+
+    // Create admin user
+    await payload.create({
+      collection: "users",
+      data: {
+        email: "testing@demo.com",
+        password: "Hello!",
+        roles: ["super-admin"],
+        username: "hello",
+        tenants: [
+          {
+            tenant: adminTenant.id,
+          },
+        ]
+      },
+    });
+
     for (const category of categories) {
         const parentCategory = await payload.create({
         collection: "categories",
