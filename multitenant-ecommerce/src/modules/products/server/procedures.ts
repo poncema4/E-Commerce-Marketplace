bf612400e21c3151cb4a55c74e7458f95ac19c17
Marco Ponce <marcpon8@gmail.com>
2025-06-25 13:55:43 -0400

    access control

--- multitenant-ecommerce/src/modules/products/server/procedures.ts
+++ multitenant-ecommerce/src/modules/products/server/procedures.ts
@@ -21,6 +21,9 @@ export const productsRouter = createTRPCRouter({
             collection: "products",
             id: input.id,
             depth: 2, // Load the product.image, product.tenant, product.tenant.image
+            select: {
+                content: false,
+            },
         });
 
         let isPurchased = false;
@@ -198,6 +201,9 @@ export const productsRouter = createTRPCRouter({
             sort,
             page: input.cursor,
             limit: input.limit,
+            select: {
+                content: false,
+            },
     });
 
     const dataWithSummarizedReviews = await Promise.all(
