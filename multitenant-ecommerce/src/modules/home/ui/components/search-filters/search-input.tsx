e39b8dfff262c8487a3f17395463cde72839b1bc
Marco Ponce <marcpon8@gmail.com>
2025-06-30 18:56:33 -0400

    deployment fix

--- multitenant-ecommerce/src/modules/home/ui/components/search-filters/search-input.tsx
+++ multitenant-ecommerce/src/modules/home/ui/components/search-filters/search-input.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react"
 import { Input } from "@/components/ui/input";
 import { CategoriesSidebar } from "./categories-sidebar";
@@ -8,29 +6,31 @@ import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
-import { useProductFilters } from "@/modules/products/hooks/use-product-filters";
 
 interface Props {
     disabled?: boolean;
-};
+    defaultValue?: string | undefined;
+    onChange?: (value: string) => void;
+}
 
 export const SearchInput = ({
     disabled,
+    defaultValue,
+    onChange,
 }: Props) => {
-    const [filters, setFilters] = useProductFilters();
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const [searchValue, setSearchValue] = useState(filters.search);
+    const [searchValue, setSearchValue] = useState(defaultValue || "");
 
     const trpc = useTRPC();
     const session = useQuery(trpc.auth.session.queryOptions());
 
     useEffect(() => {
         const timeoutId = setTimeout(() => {
-            setFilters({ search: searchValue });
+            onChange?.(searchValue)
         }, 500);
 
         return () => clearTimeout(timeoutId);
-    }, [searchValue, setFilters]);
+    }, [searchValue, onChange]);
 
     return (
         <div className="flex items-center gap-2 w-full">
